b4ce4f060994e29ed600bc9675aff59974bd1580
chuanlover <53146508+chuanlover@users.noreply.github.com>
2019-09-06 16:42:01 +0800

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,16 +1,10 @@
 var Const = {};
-// 如果你用的是 mlab 数据库， 改成 true
 Const.useMlab = false;
-//改成你自己的机器人 Token
-//Live
-// Const.botToken = "机器人ID";
-//Debug
 Const.botToken = "925816143:AAGvNZL6dCjRnKpBBHrJhJ_jN9Ura_rEQ9c";
 Const.myId = parseInt(Const.botToken.split(":")[0]);
 Const.myName = "mbitesting_bot";
 Const.myName = Const.myName.toLowerCase();
 
-//以下是峰哥的数据库服务器，可以工作，可以用，没有数据库的可以保留
 Const.restheartUrl = "https://restheart.dengnz.com/user2/";
 Const.restheartUser = "user2";
 Const.restheartPass = "changeit";
